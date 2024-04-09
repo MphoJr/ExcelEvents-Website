c437fd529c1c2934cd773634b7d95d9161a9768f
Mpho Tshawe Maudu <s221493107@mandela.ac.za>
2024-04-09 17:35:16 +0200

    Add project files.

---
+++ TRAIL/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+});
+$(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+        $(".custom-carousel .item").not($(this)).removeClass("active");
+        $(this).toggleClass("active");
+    });
+});
